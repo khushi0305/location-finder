297f5ec44e9a804ffd1ab4139bbee616bece3ff2
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-10-09 11:11:42 +0530

    ...

--- backend/connection.js
+++ backend/connection.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+const url ="mongodb+srv://khushi353:cXKfX47UexsP6gU@cluster0.bwxp2qu.mongodb.net/mydatabases?retryWrites=true&w=majority";
+mongoose.connect(url)                     
+.then((result) => {                        
+    console.log('database connected');
+}).catch((err) => {
+    console.log(err);
+});
+module.exports = mongoose;
