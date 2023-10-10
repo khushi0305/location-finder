179e40be9602b6ce0cbf1342093a006401de1153
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-10-10 12:17:11 +0530

    ...

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,65 +1,66 @@
-// import { useFormik } from 'formik';
+import { useFormik } from 'formik';
 import React from 'react'
-// import { useNavigate } from 'react-router-dom';
-// import * as Yup from "yup";
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import * as Yup from "yup";
 
-//   const SignupSchema = Yup.object().shape({
-//     name: Yup.string()
-//       .min(2, 'Too Short!')
-//       .max(50, 'Too Long!')
-//       .required('Required'),
-//     email: Yup.string().email('Invalid email').required('Email is Required'),
-//     password: Yup.string()
-//     .required('Please Enter your password')
-//     .matches(
-//       "^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$",
-//       "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
-//     ),
-//     confirm: Yup.string()
-//     .oneOf([Yup.ref('password'), null], 'Passwords must match')
-//   });
+  const SignupSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('Required'),
+    email: Yup.string().email('Invalid email').required('Email is Required'),
+    password: Yup.string()
+    .required('Please Enter your password')
+    .matches(
+      "^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$",
+      "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+    ),
+    confirm: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+  });
 
   const SignUp = () => {
-//     const navigate = useNavigate();
-//     const signupForm=useFormik({
-//       initialValues:{
-//         name : '',
-//         email : '',
-//         password : '',
-//         confirm : ''
-//       },
-//       onSubmit: async (values, {resetForm}) => {                    
-//         console.log(values);
-//       resetForm();
+    const navigate = useNavigate();
+    const signupForm=useFormik({
+      initialValues:{
+        name : '',
+        email : '',
+        password : '',
+        confirm : ''
+      },
+      onSubmit: async (values, {resetForm}) => {                    
+        console.log(values);
+      resetForm();
     
-//       const res = await fetch('http://localhost:5000/user/add', {             
-//         method: 'POST',
-//         body: JSON.stringify(values),
-//         headers: {                                    
-//           'Content-Type': 'application/json'             
-//         } 
+      const res = await fetch('http://localhost:5000/user/add', {             
+        method: 'POST',
+        body: JSON.stringify(values),
+        headers: {                                    
+          'Content-Type': 'application/json'             
+        } 
     
-//       });
-//       console.log(res.status);
-//       if(res.status === 200){
-//         Swal.fire({
-//           icon : 'success',
-//           title : 'Registered Successfully',
-//           text : 'Login to Continue!'
-//         })
-//         navigate('/login');
-//       }
-//       else{
-//         Swal.fire({
-//           icon : 'error',
-//           title: 'Error',
-//           text: 'Something went wrong!'
-//         })
-//       }
-//       },
+      });
+      console.log(res.status);
+      if(res.status === 200){
+        Swal.fire({
+          icon : 'success',
+          title : 'Registered Successfully',
+          text : 'Login to Continue!'
+        })
+        navigate('/login');
+      }
+      else{
+        Swal.fire({
+          icon : 'error',
+          title: 'Error',
+          text: 'Something went wrong!'
+        })
+      }
+      },
     
-//       validationSchema: SignupSchema
-//       })
+      validationSchema: SignupSchema
+      })
   return (
     <div className='py-5 vh-100 bg-body-secondary'>
     <div className='col-md-4 mx-auto'>
@@ -70,22 +71,22 @@ import React from 'react'
         <form >
           <label>Name: </label>
           
-          {/* <span style={{fontSize:10, marginLeft: '10px', color:'red'}}>{signupForm.touched.name && signupForm.errors.name}</span> */}
+          <span style={{fontSize:10, marginLeft: '10px', color:'red'}}>{signupForm.touched.name && signupForm.errors.name}</span>
           <input id='name' type="text" className='form-control mb-3' />
           <br/>
           <br/>
           <label>Email: </label>
-          {/* <span style={{fontSize:10, marginLeft: '10px', color:'red'}}>{signupForm.touched.email && signupForm.errors.email}</span> */}
+          <span style={{fontSize:10, marginLeft: '10px', color:'red'}}>{signupForm.touched.email && signupForm.errors.email}</span>
           <input type="text" id='email' className='form-control mb-3' />
           <br/>
 <br/>
           <label>Password: </label>
-          {/* <span style={{fontSize: 10, marginLeft: '10px', color: 'red'}}>{signupForm.touched.password && signupForm.errors.password}</span> */}
+          <span style={{fontSize: 10, marginLeft: '10px', color: 'red'}}>{signupForm.touched.password && signupForm.errors.password}</span>
           <input type="password" id='password' className='form-control mb-3' />
           <br/>
 <br/>
           <label>Confirm Password: </label>
-          {/* <span style={{fontSize: 10, marginLeft: '10px', color: 'red'}}>{signupForm.touched.confirm && signupForm.errors.confirm}</span> */}
+          <span style={{fontSize: 10, marginLeft: '10px', color: 'red'}}>{signupForm.touched.confirm && signupForm.errors.confirm}</span>
           <input type="password" id='confirm' className='form-control mb-3' />
 <br/>
 <br/>
