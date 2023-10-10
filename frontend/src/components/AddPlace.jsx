3e5144dcb172c801ac8a3cd4bb3d14b074933f5e
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-10-10 12:14:04 +0530

    ....

--- frontend/src/components/AddPlace.jsx
+++ frontend/src/components/AddPlace.jsx
@@ -17,7 +17,10 @@ const AddPlace = () => {
             <input type='text' id='describe' className='form-control'/>
 <br/>
 <br/>
-
+            <label>Image: </label>
+            <input type='' id='image'/>
+            <br/>
+            <br/>
             <button type='submit' className='btn btn-primary w-100 mt-4'>Submit</button>
         </form>
         </div>
