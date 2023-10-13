ad794e8e72098456941f644fb8d556d97d4a824d
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-10-13 10:26:59 +0530

    ....

--- frontend/src/components/AddPlace.jsx
+++ frontend/src/components/AddPlace.jsx
@@ -2,27 +2,31 @@ import React from 'react'
 
 const AddPlace = () => {
   return (
-    <div className='container'>
-        <form>
+    <div className='py-5 vh-100 bg-body-secondary'>
+    <div className='col-md-4 mx-auto'>
+      <div className='card'>
+        <div className="card-body">
+          <h2 className='my-3'>Add New Place: </h2>
+        <form >
             <label>Title: </label>
             <input type='text' id='title' className='form-control mb-3'/>
-<br/>
 <br/>
             <label>Location: </label>
             <input type='text' id= 'location' className='form-control mb-3'/>
             <br/>
-            <br/>
-
             <label>Description: </label>
             <input type='text' id='describe' className='form-control'/>
 <br/>
-<br/>
             <label>Image: </label>
-            <input type='' id='image'/>
             <br/>
+            
+            <input type='file' id='myfile'/>
             <br/>
             <button type='submit' className='btn btn-primary w-100 mt-4'>Submit</button>
         </form>
+        </div>
+        </div>
+        </div>
         </div>
   )
 }
