0bc5ede805003a03177be3db4e039bfebf2c60a4
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-10-14 13:50:33 +0530

    ....

--- frontend/src/components/DummyData.js
+++ frontend/src/components/DummyData.js
@@ -1,5 +1,34 @@
 const PlaceData = [
     {
-        title: 
+        place: 'Delhi',
+        location: 'North',
+        description: "Delhi is the capital city of India and is regarded as the heart of the nation. The city is popular for its enriched culture and heritage. The city hosts some famous historical monuments and is developing with the passing of time.",
+        image: 'https://th.bing.com/th/id/OIP.-KENXAGhkMSky-_Ni2__UAAAAA?pid=ImgDet&rs=1'
+    },
+    {
+        place: 'Uttar Pradesh',
+        location: 'North',
+        description: "Situated in the northern part of India, bordering with the capital of India New Delhi, Uttar Pradesh is one of the most popular and an established tourist destination for both Indians and non-Indians alike in India. The most populous state of India, Uttar Pradesh contains many historical monuments and places of religious significance.",
+        image: 'https://wallpapercave.com/wp/wp8009304.jpg'
+    },
+    {
+        place: 'Maharashtra',
+        location: 'West',
+        description: "Maharashtra, state of India, occupying a substantial portion of the Deccan plateau in the western peninsular part of the subcontinent. Its capital city is Mumbai, India’s most populous city as well as the country’s financial and commercial center.",
+        image: 'https://www.vdp-travels.com/wp-content/uploads/2017/08/maharashtra-2.jpg'
+    },
+    {
+        place: 'Assam',
+        location: 'East',
+        description: "Assam is a state of northeast India, located primarily in the valley of the Brahmaputra River. It was a powerful kingdom founded by invaders from Burma and China in the 13th century, before the British colonization of India1. Assam is also known for its tea, which has a rich, malty taste.",
+        image: 'https://www.incredible-northeastindia.com/images/assam-head.jpg'
+    },
+    {
+        place: 'Kerala',
+        location: 'South',
+        description: "Kerala, southwestern coastal state of India. It is a small state, constituting only about 1 percent of the total area of the country. Kerala stretches for about 360 miles along the Malabar Coast, varying in width from roughly 20 to 75 miles.",
+        image: 'https://worldmedianetwork.uk/wp-content/uploads/2020/08/KERALA-1024x768.jpg'
     }
 ]
+
+export default PlaceData;
