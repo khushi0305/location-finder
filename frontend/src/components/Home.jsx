8498b738fb7277752f17ce8e4f6fb8468f07f7f9
Khushi Srivastava <srivastavakhushi518@bbdnitm.ac.in>
2023-10-15 15:57:01 +0530

    ...

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,8 +4,9 @@ const Home = () => {
   return (
     <div>
       <img className='myimg' src='https://www.exoticdestinations.com.au/wp-content/uploads/2019/11/best-places-to-visit-in-india.jpg'/>
-    <h1 className='text-center fw-bold'>Location Finder</h1>
-    <p>CityScope is an online site that helps tourist know about famous tourist attractions, excursions, and countless other things to do on your next vacation or weekend adventure. We work together with selected activity providers from all over the world. The activity providers are the ones offering the activities.</p>
+    <h1 className='text-center fw-bold'>CityScope</h1>
+    <h3 className='text-center text-secondary'>Location Finder</h3>
+    <p><b>CityScope</b> is an online site that helps tourist know about famous tourist attractions, excursions, and countless other things to do on your next vacation or weekend adventure. We work together with selected activity providers from all over India. The activity providers are the ones offering the activities.</p>
     </div>
   )
 }
